5be930097b4d7dd13b0162795c2ce033a8fdc878
keach_murakami <keach_murakami@2023-40-00007s-MacBook-Pro.local>
2024-01-23 14:17:41 +0900

    ✨: 年跨ぎでのデータ取得を実装 ✨: OPeNDAP接続先を引数として可変に変更

--- R/configuration.R
+++ R/configuration.R
@@ -5,9 +5,10 @@
 #' @export
 #' @param userid your user id issued by the development group
 #' @param password your password issued by the development group
+#' @param server URL for OPeNDAP server
 #' @param .force logical. pass verification for offline .onLoad
 amgsds_config <-
-  function(userid = Sys.getenv("amgsds_id"), password = Sys.getenv("amgsds_pw"), .force = FALSE){
+  function(userid = Sys.getenv("amgsds_id"), password = Sys.getenv("amgsds_pw"), server = "amd.rd.naro.go.jp", .force = FALSE){
 
     is_network_available <- TRUE
 
@@ -34,7 +35,7 @@ amgsds_config <-
     if(is_network_available){
       # verify id/pw
       tryCatch({
-        connect <- readr::read_lines(stringr::str_glue("https://{userid}:{password}@amd.rd.naro.go.jp/opendap/AMD/"))
+        connect <- readr::read_lines(stringr::str_glue("https://{userid}:{password}@{server}/opendap/AMD/"))
         usethis::ui_done("USERID and PASSWORD -> verified\n")
       },
       error = function(e){
