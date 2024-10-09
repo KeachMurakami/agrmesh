7d6566dfcbf14f8ce893675c7c5b36f18da8e537
keach_murakami <keach_murakami@2023-40-00007s-MacBook-Pro.local>
2024-10-09 13:47:37 +0900

    🔖: added codes of valid meshes in AMS

--- R/data.R
+++ R/data.R
@@ -4,3 +4,10 @@ globalVariables("nonempty_meshes")
 #'
 #' @format A numeric vector with 304 meshes:
 "nonempty_meshes"
+
+globalVariables("nonempty_meshes_fewer")
+
+#' List of primary mesh codes with valid grid values in scenario datasets
+#'
+#' @format A numeric vector with 166 meshes:
+"nonempty_meshes_fewer"
