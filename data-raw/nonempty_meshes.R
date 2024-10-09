00cca2fa595ba896836af7c56873652922f5a715
keach_murakami <keach_murakami@2023-40-00007s-MacBook-Pro.local>
2024-10-09 13:48:31 +0900

    🔖: added codes of valid meshes in AMS

--- data-raw/nonempty_meshes.R
+++ data-raw/nonempty_meshes.R
@@ -7,4 +7,14 @@ nonempty_meshes <-
   stringr::str_extract("(p....e)") %>%
   readr::parse_number()
 
+nonempty_meshes_fewer <-
+  stringr::str_glue("https://{Sys.getenv('amgsds_id')}:{Sys.getenv('amgsds_pw')}@amd.rd.naro.go.jp/opendap/AMS/MIROC5/historical/2005/eAPCP/contents.html") %>%
+  rvest::read_html() %>%
+  rvest::html_nodes("a") %>%
+  rvest::html_text() %>%
+  stringr::str_subset("p....e") %>%
+  stringr::str_extract("(p....e)") %>%
+  readr::parse_number()
+
 usethis::use_data(nonempty_meshes, overwrite = TRUE)
+usethis::use_data(nonempty_meshes_fewer, overwrite = TRUE)
